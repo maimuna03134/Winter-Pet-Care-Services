8f3de50b9e004d92452c82ff687df677ea132cb7
maimuna03134 <mj9644212@gmail.com>
2025-12-03 00:44:44 +0600

    update nevbar and added nav route like about us, pricing, contact

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,146 +1,211 @@
 import React, { use } from "react";
-import userImg from '../assets/user.png'
-
+import userImg from "../assets/user.png";
 import MyLink from "./link/MyLink";
 import { Link } from "react-router";
 import MyContainer from "./container/MyContainer";
 import { AuthContext } from "../provider/AuthProvider";
 import toast from "react-hot-toast";
+import { GoHomeFill } from "react-icons/go";
+import { MdMiscellaneousServices } from "react-icons/md";
+import { FaDollarSign, FaEnvelope, FaInfoCircle, FaUser } from "react-icons/fa";
+import { IoLogIn, IoLogOut } from "react-icons/io5";
 
 const Navbar = () => {
   const { user, logOut } = use(AuthContext);
   const handleLogOut = () => {
     // console.log('user trying to logout')
 
-    logOut().then(() => {
-      toast.success("Your Logged Out successful!");
-    }).catch((err) => {
-      console.log(err.message);
-    })
-  }
-
+    logOut()
+      .then(() => {
+        toast.success("Logged out successfully!");
+      })
+      .catch((err) => {
+        toast.error(err.message);
+      });
+  };
 
   return (
-    <div className="shadow-lg bg-base-100">
-      <MyContainer className={"navbar"}>
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+    <>
+      <div className="fixed top-0 left-0 right-0 z-50 shadow-lg bg-base-100">
+        <MyContainer className={"navbar"}>
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-9999 mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <MyLink to={"/"}>Home</MyLink>
-              </li>
-              <li>
-                <MyLink to={"/services"}>Services</MyLink>
-              </li>
-              <li>
-                <MyLink to={"/profile"}> My Profile</MyLink>
-              </li>
-              {user ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-50 mt-3 w-52 p-2 shadow"
+              >
                 <li>
-                  <button
-                    onClick={handleLogOut}
-                    className="w-full text-left py-1"
-                  >
-                    Logout
-                  </button>
+                  <MyLink to={"/"}>
+                    <GoHomeFill /> Home
+                  </MyLink>
                 </li>
-              ) : (
-                <>
+                <li>
+                  <MyLink to={"/services"}>
+                    <MdMiscellaneousServices /> Services
+                  </MyLink>
+                </li>
+                <li>
+                  <MyLink to={"/services"}>
+                    <FaInfoCircle /> About Us
+                  </MyLink>
+                </li>
+                <li>
+                  <MyLink to={"/services"}>
+                    <FaDollarSign /> Pricing
+                  </MyLink>
+                </li>
+                <li>
+                  <MyLink to={"/services"}>
+                    <FaEnvelope /> Contact
+                  </MyLink>
+                </li>
+                {user && (
                   <li>
-                    <Link to="/auth/login" className="w-full text-left py-1 ">
-                      Login
-                    </Link>
+                    <MyLink to={"/profile"}>
+                      <FaUser /> My Profile
+                    </MyLink>
                   </li>
+                )}
+
+                {user ? (
                   <li>
-                    <Link to="/auth/register" className="w-full text-left py-1">
-                      Register
-                    </Link>
+                    <button
+                      onClick={handleLogOut}
+                      className="w-full text-left py-1 flex items-center gap-2"
+                    >
+                      <IoLogOut /> Logout
+                    </button>
                   </li>
-                </>
-              )}
-            </ul>
-          </div>
-          <Link to="/" className="flex items-center justify-center">
-            <div className=" bg-linear-to-r from-orange-300 to-orange-100 rounded-full ">
+                ) : (
+                  <>
+                    <li>
+                      <Link
+                        to="/auth/login"
+                        className="w-full text-left py-1 flex items-center gao-2"
+                      >
+                        <IoLogIn /> Login
+                      </Link>
+                    </li>
+                  </>
+                )}
+              </ul>
+            </div>
+            <div className=" bg-linear-to-r from-orange-300 to-orange-100 rounded-full flex items-center justify-center">
               <span className="text-white text-2xl">🐱</span>
             </div>
             <a className="ml-2 font-bold text-xl text-[#031b4b]">WarmPaws</a>
-          </Link>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <MyLink to={"/"}>Home</MyLink>
-            </li>
-            <li>
-              <MyLink to={"/services"}>Services</MyLink>
-            </li>
-            <li>
-              <MyLink to={"/profile"}> My Profile</MyLink>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-end space-x-2">
-          <div className="relative group">
-            <img
-              src={user?.photoURL || userImg}
-              alt=""
-              className="w-[35px] rounded-full"
-            />
-
-            {user && (
-              <span className="absolute left-1/2 -translate-x-1/2 bg-gray-800 text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap">
-                {user.displayName}
-              </span>
-            )}
           </div>
-          {user ? (
-            <>
-              <button
-                onClick={handleLogOut}
-                className="btn btn-style hidden lg:flex items-center justify-center"
-              >
-                LogOut
-              </button>
-            </>
-          ) : (
-            <>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-2">
+              <li>
+                <MyLink to={"/"}>
+                  <GoHomeFill /> Home
+                </MyLink>
+              </li>
+              <li>
+                <MyLink to={"/services"}>
+                  <MdMiscellaneousServices /> Services
+                </MyLink>
+              </li>
+              <li>
+                <MyLink to={"/about-us"}>
+                  <FaInfoCircle /> About Us
+                </MyLink>
+              </li>
+              <li>
+                <MyLink to={"/pricing"}>
+                  <FaDollarSign /> Pricing
+                </MyLink>
+              </li>
+              <li>
+                <MyLink to={"/contact"}>
+                  <FaEnvelope /> Contact
+                </MyLink>
+              </li>
+              {/* {user && (
+              <li>
+                <MyLink to={"/profile"}>
+                  <FaUser /> My Profile
+                </MyLink>
+              </li>
+            )} */}
+            </ul>
+          </div>
+          <div className="navbar-end">
+            {user ? (
+              <div className="dropdown dropdown-end">
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost btn-circle avatar"
+                >
+                  <div className="w-9 border-2 border-gray-300 rounded-full">
+                    <img
+                      alt="User avatar"
+                      src={user?.photoURL || userImg}
+                      className="rounded-full"
+                    />
+                  </div>
+                </div>
+                <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content bg-base-100 rounded-box z-50 mt-3 w-52 p-2 shadow"
+                >
+                  <div className="pb-3 border-b border-b-gray-200">
+                    <li className="text-sm font-bold px-4 py-1">
+                      {user.displayName}
+                    </li>
+                    <li className="text-xs px-4">{user.email}</li>
+                  </div>
+                  <li className="mt-3">
+                    <Link to={"/profile"}>
+                      <FaUser /> Profile
+                    </Link>
+                  </li>
+                  <li>
+                    <button
+                      onClick={handleLogOut}
+                      className="btn btn-xs text-left bg-linear-to-r from-orange-400 to-orange-600 text-white border-none"
+                    >
+                      <IoLogOut /> Logout
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            ) : (
               <Link
                 to="/auth/login"
-                className="btn btn-style hidden lg:flex items-center justify-center "
+                className="btn btn-sm rounded-full bg-linear-to-r from-orange-400 to-orange-600 text-white border-none"
               >
-                Login
+                <IoLogIn /> Login
               </Link>
-              <Link
-                to="/auth/register"
-                className="btn btn-style hidden lg:flex items-center justify-center"
-              >
-                Register
-              </Link>
-            </>
-          )}
-        </div>
-      </MyContainer>
-    </div>
+            )}
+          </div>
+        </MyContainer>
+      </div>
+      {/* Spacer div to prevent content from hiding under fixed navbar */}
+      <div className="h-16"></div>
+    </>
   );
 };
 
